6117a459ec8b47396e0fb78aace0fe3a1d5a6e84
unknown <benjaminmick@gmail.com>
2022-05-21 16:52:53 -0600

    comments to explain how it works

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -50,6 +50,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   _getUserMedia() async {
     final Map<String, dynamic> mediaConstraints = {
+      //TODO send data
       'audio': true,
       'video': {
         'facingMode': 'user',
@@ -66,7 +67,7 @@ class _MyHomePageState extends State<MyHomePage> {
   _createPeerConnecion() async {
     Map<String, dynamic> configuration = {
       "iceServers": [
-        {"url": "stun:stun.l.google.com:19302"},
+        {"url": "stun:stun.l.google.com:19302"}, //TODO add to our server
       ]
     };
 
@@ -107,6 +108,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return pc;
   }
 
+//TODO understand and comment how these work
   void _createOffer() async {
     RTCSessionDescription description =
         await _peerConnection!.createOffer({'offerToReceiveVideo': 1});
@@ -118,16 +120,17 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void _createAnswer() async {
-    RTCSessionDescription description =
+    RTCSessionDescription description = //a description of the connection
         await _peerConnection!.createAnswer({'offerToReceiveVideo': 1});
-
+//creates answer based on offer
     var session = parse(description.sdp.toString());
     print(json.encode(session));
 
-    _peerConnection!.setLocalDescription(description);
+    _peerConnection!.setLocalDescription(description); //changes the description
   }
 
   void _setRemoteDescription() async {
+    //creates the description of the remote user
     String jsonString = sdpController.text;
     dynamic session = await jsonDecode(jsonString);
 
@@ -135,18 +138,20 @@ class _MyHomePageState extends State<MyHomePage> {
 
     RTCSessionDescription description =
         RTCSessionDescription(sdp, _offer ? 'answer' : 'offer');
-    print(description.toMap());
+    print(description.toMap()); //puts the description in the console for us to
+    //cop and paste into the text boxx
 
-    await _peerConnection!.setRemoteDescription(description);
+    await _peerConnection!.setRemoteDescription(description); //allows the code
+    //to keep running while it is working
   }
 
   void _addCandidate() async {
     String jsonString = sdpController.text;
     dynamic session = await jsonDecode(jsonString);
-    print(session['candidate']);
+    print(session['candidate']); //puts the candidates into the console
     dynamic candidate = RTCIceCandidate(
         session['candidate'], session['sdpMid'], session['sdpMlineIndex']);
-    await _peerConnection!.addCandidate(candidate);
+    await _peerConnection!.addCandidate(candidate); //allows code to continue
   }
 
   @override
@@ -155,7 +160,6 @@ class _MyHomePageState extends State<MyHomePage> {
     _createPeerConnecion().then((pc) {
       _peerConnection = pc;
     });
-    // _getUserMedia();
     super.initState();
   }
 
@@ -167,22 +171,26 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   SizedBox videoRenderers() => SizedBox(
+        // creates the places where screens are displayed
         height: 210,
         child: Row(children: [
           Flexible(
             child: Container(
               key: const Key('local'),
               margin: const EdgeInsets.fromLTRB(5.0, 5.0, 5.0, 5.0),
-              decoration: const BoxDecoration(color: Colors.black),
-              child: RTCVideoView(_localVideoRenderer),
+              decoration: const BoxDecoration(color: Colors.red),
+              //sets the default screen color to black
+              child: RTCVideoView(_localVideoRenderer), //puts your video on box
             ),
           ),
           Flexible(
             child: Container(
               key: const Key('remote'),
               margin: const EdgeInsets.fromLTRB(5.0, 5.0, 5.0, 5.0),
-              decoration: const BoxDecoration(color: Colors.black),
-              child: RTCVideoView(_remoteVideoRenderer),
+              decoration: const BoxDecoration(color: Colors.red),
+              // sets default box color to black
+              child:
+                  RTCVideoView(_remoteVideoRenderer), //puts peer's video on box
             ),
           ),
         ]),
@@ -206,8 +214,10 @@ class _MyHomePageState extends State<MyHomePage> {
                     child: TextField(
                       controller: sdpController,
                       keyboardType: TextInputType.multiline,
-                      maxLines: 4,
-                      maxLength: TextField.noMaxLength,
+                      maxLines: 4, //determines the maximun amount of lines
+                      //displayed on the screen at any time
+                      maxLength: TextField.noMaxLength, // makes the text field
+                      //able to hold unlimited text
                     ),
                   ),
                 ),
@@ -215,12 +225,15 @@ class _MyHomePageState extends State<MyHomePage> {
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     ElevatedButton(
-                      onPressed: _createOffer,
-                      child: const Text("Offer"),
+                      //creates button
+                      onPressed: _createOffer, //when pressed, it creates offer
+                      child:
+                          const Text("Offer"), //display the text on the button
                     ),
                     const SizedBox(
+                      //creates box for button
                       height: 10,
-                    ),
+                    ), // the rest of these are the same thing
                     ElevatedButton(
                       onPressed: _createAnswer,
                       child: const Text("Answer"),
