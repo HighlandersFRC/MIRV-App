15f2de543b101fa36b124b7036a38c0f11731350
unknown <benjaminmick@gmail.com>
2022-06-08 11:17:13 -0600

    added second page

--- android/app/src/main/kotlin/com/example/test/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/test/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.webrtc_flutter
+package com.example.test
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_webrtc/flutter_webrtc.dart';
-import 'package:sdp_transform/sdp_transform.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,10 +12,26 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
+  }
+}
+
+class MirvApi {
+  String firstRover = "one";
+  String secondRover = "two";
+  String thirdRover = "three";
+  String _authToken = "auth token";
+
+  String getAuthToken() {
+    _authToken = "new auth token";
+    return "auth token";
+  }
+
+  List<String> getRovers() {
+    return <String>[firstRover, secondRover, thirdRover];
   }
 }
 
@@ -33,229 +45,110 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final _localVideoRenderer = RTCVideoRenderer();
-  final _remoteVideoRenderer = RTCVideoRenderer();
-  final sdpController = TextEditingController();
+  List<String> entries = <String>[];
+  MirvApi mirvApi = MirvApi();
+  var roverList = <String>['1', '2', '3'];
 
-  bool _offer = false;
-
-  RTCPeerConnection? _peerConnection;
-  MediaStream? _localStream;
-
-  initRenderer() async {
-    await _localVideoRenderer.initialize();
-    await _remoteVideoRenderer.initialize();
-  }
-
-  _getUserMedia() async {
-    final Map<String, dynamic> mediaConstraints = {
-      //TODO send data
-      'audio': true,
-      'video': {
-        'facingMode': 'user',
-      }
-    };
-
-    MediaStream stream =
-        await navigator.mediaDevices.getUserMedia(mediaConstraints);
-
-    _localVideoRenderer.srcObject = stream;
-    return stream;
-  }
-
-  _createPeerConnecion() async {
-    Map<String, dynamic> configuration = {
-      "iceServers": [
-        {"url": "stun:stun.l.google.com:19302"}, //TODO add to our server
-      ]
-    };
-
-    final Map<String, dynamic> offerSdpConstraints = {
-      "mandatory": {
-        "OfferToReceiveAudio": true,
-        "OfferToReceiveVideo": true,
-      },
-      "optional": [],
-    };
-
-    _localStream = await _getUserMedia();
-
-    RTCPeerConnection pc =
-        await createPeerConnection(configuration, offerSdpConstraints);
-
-    pc.addStream(_localStream!);
-
-    pc.onIceCandidate = (e) {
-      if (e.candidate != null) {
-        print(json.encode({
-          'candidate': e.candidate.toString(),
-          'sdpMid': e.sdpMid.toString(),
-          'sdpMlineIndex': e.sdpMLineIndex,
-        }));
-      }
-    };
-
-    pc.onIceConnectionState = (e) {
-      print(e);
-    };
-
-    pc.onAddStream = (stream) {
-      print('addStream: ' + stream.id);
-      _remoteVideoRenderer.srcObject = stream;
-    };
-
-    return pc;
-  }
-
-//TODO understand and comment how these work
-  void _createOffer() async {
-    RTCSessionDescription description =
-        await _peerConnection!.createOffer({'offerToReceiveVideo': 1});
-    var session = parse(description.sdp.toString());
-    print(json.encode(session));
-    _offer = true;
-
-    _peerConnection!.setLocalDescription(description);
-  }
-
-  void _createAnswer() async {
-    RTCSessionDescription description = //a description of the connection
-        await _peerConnection!.createAnswer({'offerToReceiveVideo': 1});
-//creates answer based on offer
-    var session = parse(description.sdp.toString());
-    print(json.encode(session));
-
-    _peerConnection!.setLocalDescription(description); //changes the description
-  }
-
-  void _setRemoteDescription() async {
-    //creates the description of the remote user
-    String jsonString = sdpController.text;
-    dynamic session = await jsonDecode(jsonString);
-
-    String sdp = write(session, null);
-
-    RTCSessionDescription description =
-        RTCSessionDescription(sdp, _offer ? 'answer' : 'offer');
-    print(description.toMap()); //puts the description in the console for us to
-    //cop and paste into the text boxx
-
-    await _peerConnection!.setRemoteDescription(description); //allows the code
-    //to keep running while it is working
-  }
-
-  void _addCandidate() async {
-    String jsonString = sdpController.text;
-    dynamic session = await jsonDecode(jsonString);
-    print(session['candidate']); //puts the candidates into the console
-    dynamic candidate = RTCIceCandidate(
-        session['candidate'], session['sdpMid'], session['sdpMlineIndex']);
-    await _peerConnection!.addCandidate(candidate); //allows code to continue
-  }
-
-  @override
-  void initState() {
-    initRenderer();
-    _createPeerConnecion().then((pc) {
-      _peerConnection = pc;
+  void _refreshRoversList() {
+    entries = mirvApi.getRovers();
+    setState(() {
+      entries;
     });
-    super.initState();
   }
-
-  @override
-  void dispose() async {
-    await _localVideoRenderer.dispose();
-    sdpController.dispose();
-    super.dispose();
-  }
-
-  SizedBox videoRenderers() => SizedBox(
-        // creates the places where screens are displayed
-        height: 210,
-        child: Row(children: [
-          Flexible(
-            child: Container(
-              key: const Key('local'),
-              margin: const EdgeInsets.fromLTRB(5.0, 5.0, 5.0, 5.0),
-              decoration: const BoxDecoration(color: Colors.red),
-              //sets the default screen color to black
-              child: RTCVideoView(_localVideoRenderer), //puts your video on box
-            ),
-          ),
-          Flexible(
-            child: Container(
-              key: const Key('remote'),
-              margin: const EdgeInsets.fromLTRB(5.0, 5.0, 5.0, 5.0),
-              decoration: const BoxDecoration(color: Colors.red),
-              // sets default box color to black
-              child:
-                  RTCVideoView(_remoteVideoRenderer), //puts peer's video on box
-            ),
-          ),
-        ]),
-      );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Color.fromARGB(255, 250, 250, 250),
         appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Column(
-          children: [
-            videoRenderers(),
-            Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.5,
-                    child: TextField(
-                      controller: sdpController,
-                      keyboardType: TextInputType.multiline,
-                      maxLines: 4, //determines the maximun amount of lines
-                      //displayed on the screen at any time
-                      maxLength: TextField.noMaxLength, // makes the text field
-                      //able to hold unlimited text
+          foregroundColor: Color.fromARGB(255, 0, 0, 0),
+          title: const Text(
+            "Rover Selection",
+            textScaleFactor: 2,
+          ),
+          actions: <Widget>[
+            Padding(
+                padding: const EdgeInsets.only(right: 50.0),
+                child: GestureDetector(
+                  onTap: _refreshRoversList,
+                  child: const Icon(Icons.replay_circle_filled_sharp,
+                      size: 50.0, color: Color.fromARGB(199, 30, 0, 0)),
+                )),
+          ],
+        ), //appbar
+        body: ListView.builder(
+          itemBuilder: (BuildContext context, int index) {
+            void _pressedButton() {
+              late String pageOfRover = "RoverPage${roverList[index]}";
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => RoverPage(roverID: roverList[index])),
+              );
+            }
+
+            return Container(
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 220, 220, 220),
+                    border: Border.all(
+                        width: 10, color: Color.fromARGB(255, 250, 250, 250)),
+                    borderRadius: const BorderRadius.all(
+                        const Radius.elliptical(30, 25))),
+                child: ListTile(
+                  trailing: ElevatedButton(
+                    onPressed: _pressedButton,
+                    child: const Icon(
+                      Icons.arrow_right_alt_rounded,
+                      size: 50,
+                      color: Color.fromARGB(255, 30, 0, 0),
                     ),
                   ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                      //creates button
-                      onPressed: _createOffer, //when pressed, it creates offer
-                      child:
-                          const Text("Offer"), //display the text on the button
-                    ),
-                    const SizedBox(
-                      //creates box for button
-                      height: 10,
-                    ), // the rest of these are the same thing
-                    ElevatedButton(
-                      onPressed: _createAnswer,
-                      child: const Text("Answer"),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    ElevatedButton(
-                      onPressed: _setRemoteDescription,
-                      child: const Text("Set Remote Description"),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    ElevatedButton(
-                      onPressed: _addCandidate,
-                      child: const Text("Set Candidate"),
-                    ),
-                  ],
-                )
-              ],
-            ),
-          ],
+                  title: Text(
+                    "Rover ${roverList[index]}",
+                    textAlign: TextAlign.left,
+                    textScaleFactor: 3,
+                    textHeightBehavior: const TextHeightBehavior(
+                        applyHeightToFirstAscent: true),
+                  ),
+                ));
+          },
+          itemCount: roverList.length,
         ));
+  }
+}
+
+class RoverPage extends StatelessWidget {
+  final String roverID;
+  RoverPage({Key key, required this.roverID}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    void _goBack() {
+      Navigator.pop(context);
+    }
+
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 250, 250, 250),
+      appBar: AppBar(
+        title: const Text(
+          'Rover Control',
+          textScaleFactor: 2,
+        ),
+        backgroundColor: Colors.blue,
+        foregroundColor: Color.fromARGB(255, 30, 0, 0),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            color: Color.fromARGB(255, 220, 220, 220),
+            border: Border.all(
+                width: 50, color: Color.fromARGB(255, 250, 250, 250)),
+            borderRadius: const BorderRadius.all(const Radius.circular(70))),
+        child: Text(
+          "Video Here",
+          textScaleFactor: 1.7,
+        ),
+        margin: EdgeInsets.only(bottom: 400),
+      ),
+    );
   }
 }
