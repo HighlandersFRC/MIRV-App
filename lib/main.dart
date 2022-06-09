789519621624e395491ad3c9caff1590139b340b
ben-mick <benjaminmick@gmail.com>
2022-06-09 14:48:48 -0600

    added joystick

--- lib/main.dart
+++ lib/main.dart
@@ -59,9 +59,9 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color.fromARGB(255, 250, 250, 250),
+        backgroundColor: const Color.fromARGB(255, 250, 250, 250),
         appBar: AppBar(
-          foregroundColor: Color.fromARGB(255, 0, 0, 0),
+          foregroundColor: const Color.fromARGB(255, 0, 0, 0),
           title: const Text(
             "Rover Selection",
             textScaleFactor: 2,
@@ -79,7 +79,6 @@ class _MyHomePageState extends State<MyHomePage> {
         body: ListView.builder(
           itemBuilder: (BuildContext context, int index) {
             void _pressedButton() {
-              late String pageOfRover = "RoverPage${roverList[index]}";
               Navigator.push(
                 context,
                 MaterialPageRoute(
@@ -89,11 +88,12 @@ class _MyHomePageState extends State<MyHomePage> {
 
             return Container(
                 decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 220, 220, 220),
+                    color: const Color.fromARGB(255, 220, 220, 220),
                     border: Border.all(
-                        width: 10, color: Color.fromARGB(255, 250, 250, 250)),
-                    borderRadius: const BorderRadius.all(
-                        const Radius.elliptical(30, 25))),
+                        width: 10,
+                        color: const Color.fromARGB(255, 250, 250, 250)),
+                    borderRadius:
+                        const BorderRadius.all(Radius.elliptical(30, 25))),
                 child: ListTile(
                   trailing: ElevatedButton(
                     onPressed: _pressedButton,
@@ -119,35 +119,31 @@ class _MyHomePageState extends State<MyHomePage> {
 
 class RoverPage extends StatelessWidget {
   final String roverID;
-  RoverPage({Key key, required this.roverID}) : super(key: key);
+  const RoverPage({Key? key, required this.roverID}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    void _goBack() {
-      Navigator.pop(context);
-    }
-
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 250, 250, 250),
+      backgroundColor: const Color.fromARGB(255, 250, 250, 250),
       appBar: AppBar(
-        title: const Text(
-          'Rover Control',
+        title: Text(
+          'Rover Control $roverID',
           textScaleFactor: 2,
         ),
         backgroundColor: Colors.blue,
-        foregroundColor: Color.fromARGB(255, 30, 0, 0),
+        foregroundColor: const Color.fromARGB(255, 30, 0, 0),
       ),
       body: Container(
         alignment: Alignment.center,
         decoration: BoxDecoration(
-            color: Color.fromARGB(255, 220, 220, 220),
+            color: const Color.fromARGB(255, 220, 220, 220),
             border: Border.all(
-                width: 50, color: Color.fromARGB(255, 250, 250, 250)),
-            borderRadius: const BorderRadius.all(const Radius.circular(70))),
-        child: Text(
+                width: 50, color: const Color.fromARGB(255, 250, 250, 250)),
+            borderRadius: const BorderRadius.all(Radius.circular(70))),
+        margin: const EdgeInsets.only(bottom: 400),
+        child: const Text(
           "Video Here",
           textScaleFactor: 1.7,
         ),
-        margin: EdgeInsets.only(bottom: 400),
       ),
     );
   }
