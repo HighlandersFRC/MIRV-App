03a0a396b6e31d5593b74bffd2869a8743255225
H-Hailey <Squirt@joshholman.info>
2022-06-15 11:35:02 -0600

    added Icons to identify status of rovers

--- lib/ui/screens/rover_selection_page.dart
+++ lib/ui/screens/rover_selection_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:test/models/rover_state_type.dart';
+import 'package:test/models/rover_status_type.dart';
 import 'package:test/models/rover_summary.dart';
 import 'package:test/services/mirv_api.dart';
 import 'package:test/ui/screens/rover_operation_page.dart';
@@ -18,6 +20,42 @@ class _RoverSelectionPageState extends State<RoverSelectionPage> {
     roverList.value = await mirvApi.getRovers();
   }
 
+  Icon _batteryIcon(int batteryLevel, {int? alertLevel}) {
+    double divisor = 100 / 7;
+    int result = (batteryLevel / divisor).ceil();
+    if (alertLevel != null && batteryLevel < alertLevel) {
+      return Icon(Icons.battery_alert_rounded);
+    }
+    switch (result) {
+      case 0:
+        return Icon(Icons.battery_0_bar_rounded);
+      case 1:
+        return Icon(Icons.battery_1_bar_rounded);
+      case 2:
+        return Icon(Icons.battery_2_bar_rounded);
+      case 3:
+        return Icon(Icons.battery_3_bar_rounded);
+      case 4:
+        return Icon(Icons.battery_4_bar_rounded);
+      case 5:
+        return Icon(Icons.battery_5_bar_rounded);
+      case 6:
+        return Icon(Icons.battery_6_bar_rounded);
+      default:
+        return Icon(Icons.battery_full_rounded);
+    }
+  }
+
+  Icon _getStatusIcon(RoverStatusType value) {
+    switch (value) {
+      case RoverStatusType.available:
+        return Icon(Icons.lock_open_rounded);
+
+      case RoverStatusType.unavailable:
+        return Icon(Icons.lock_rounded);
+    }
+  }
+
   @override
   void initState() {
     super.initState();
@@ -27,59 +65,53 @@ class _RoverSelectionPageState extends State<RoverSelectionPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color.fromARGB(255, 250, 250, 250),
-        appBar: AppBar(
-          foregroundColor: Color.fromARGB(255, 0, 0, 0),
-          title: const Text(
-            "Rover Selection",
-            textScaleFactor: 2,
-          ),
-          actions: <Widget>[
-            IconButton(
-                onPressed: _refreshRoversList,
-                icon: const Icon(Icons.refresh_rounded, size: 45))
-          ],
-        ), //appbar
-        body: Obx(
-          () => ListView.builder(
-            itemBuilder: (BuildContext context, int index) {
-              void _pressedButton() {
-                late String pageOfRover = "RoverPage${roverList[index]}";
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => RoverOperationPage(
-                          roverID: roverList[index].roverId)),
-                );
-              }
-
-              return Container(
-                  decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 220, 220, 220),
-                      border: Border.all(
-                          width: 10, color: Color.fromARGB(255, 250, 250, 250)),
-                      borderRadius: const BorderRadius.all(
-                          const Radius.elliptical(30, 25))),
-                  child: ListTile(
-                    trailing: ElevatedButton(
-                      onPressed: _pressedButton,
-                      child: const Icon(
-                        Icons.arrow_right_alt_rounded,
-                        size: 50,
-                        color: Color.fromARGB(255, 30, 0, 0),
-                      ),
-                    ),
-                    title: Text(
-                      "Rover ${roverList[index]}",
-                      textAlign: TextAlign.left,
-                      textScaleFactor: 3,
-                      textHeightBehavior: const TextHeightBehavior(
-                          applyHeightToFirstAscent: true),
-                    ),
-                  ));
-            },
-            itemCount: roverList.length,
-          ),
-        ));
+      backgroundColor: Color.fromARGB(255, 250, 250, 250),
+      appBar: AppBar(
+        foregroundColor: Color.fromARGB(255, 0, 0, 0),
+        title: const Text(
+          "Rover Selection",
+        ),
+        actions: <Widget>[
+          IconButton(
+              onPressed: _refreshRoversList,
+              icon: const Icon(Icons.refresh_rounded, size: 45))
+        ],
+      ), //appbar
+      body: Obx(
+        () => ListView.builder(
+          itemCount: roverList.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 1,
+                horizontal: 4,
+              ),
+              child: ListTile(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => RoverOperationPage(
+                            roverID: roverList[index].roverId)),
+                  );
+                },
+                title: Text(
+                  "Rover ${roverList[index].roverId}",
+                ),
+                subtitle: Text(
+                    'Battery ${roverList[index].battery.toString()} \n ${roverList[index].state}'),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    _getStatusIcon(roverList[index].status),
+                    _batteryIcon(roverList[index].battery, alertLevel: 20),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
