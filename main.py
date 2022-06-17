1d400e7a86bfe48b5e79a4fae3758b14cb36e242
jwiens <jwiens@neaeraconsulting.com>
2022-06-17 11:00:33 -0600

    Cleaned up Unneccessary python app components

--- main.py
+++ main.py
@@ -33,75 +33,15 @@ class FlagVideoStreamTrack(VideoStreamTrack):
 
     def __init__(self):
         super().__init__()  # don't forget this!
-        self.counter = 0
-        height, width = 480, 640
-
-        # generate flag
-        data_bgr = np.hstack(
-            [
-                self._create_rectangle(
-                    width=213, height=480, color=(255, 0, 0)
-                ),  # blue
-                self._create_rectangle(
-                    width=214, height=480, color=(255, 255, 255)
-                ),  # white
-                self._create_rectangle(width=213, height=480, color=(0, 0, 255)),  # red
-            ]
-        )
-
-        # shrink and center it
-        M = np.float32([[0.5, 0, width / 4], [0, 0.5, height / 4]])
-        data_bgr = cv2.warpAffine(data_bgr, M, (width, height))
-
-        # compute animation
-        omega = 2 * math.pi / height
-        id_x = np.tile(np.array(range(width), dtype=np.float32), (height, 1))
-        id_y = np.tile(
-            np.array(range(height), dtype=np.float32), (width, 1)
-        ).transpose()
-
-        self.frames = []
-        for k in range(30):
-            phase = 2 * k * math.pi / 30
-            map_x = id_x + 10 * np.cos(omega * id_x + phase)
-            map_y = id_y + 10 * np.sin(omega * id_x + phase)
-            self.frames.append(
-                VideoFrame.from_ndarray(
-                    cv2.remap(data_bgr, map_x, map_y, cv2.INTER_LINEAR), format="bgr24"
-                )
-            )
         
-
     async def recv(self):
         ret, frame = cap.read()
         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
         frame = VideoFrame.from_ndarray(frame)
         pts, time_base = await self.next_timestamp()
-        # frame = self.frames[self.counter % 30]
         frame.pts = pts
         frame.time_base = time_base
-        self.counter += 1
-
-
-        
-        
         return frame
-
-    def _create_rectangle(self, width, height, color):
-        data_bgr = np.zeros((height, width, 3), np.uint8)
-        data_bgr[:, :] = color
-        return data_bgr
-
-
-async def index(request):
-    content = open(os.path.join(ROOT, "index.html"), "r").read()
-    return web.Response(content_type="text/html", text=content)
-
-
-async def javascript(request):
-    content = open(os.path.join(ROOT, "client.js"), "r").read()
-    return web.Response(content_type="application/javascript", text=content)
-
 
 
 # Website posts an offer to python server
@@ -115,15 +55,6 @@ async def offer(request):
     pc.addTrack(FlagVideoStreamTrack())
     pc_id = "PeerConnection(%s)" % uuid.uuid4()
     pcs.add(pc)
-
-    # def log_info(msg, *args):
-    #     logger.info(pc_id + " " + msg, *args)
-
-    # log_info("Created for %s", request.remote)
-
-    # # prepare local media
-    # player = MediaPlayer(os.path.join(ROOT, "demo-instruct.wav"))
-    # recorder = MediaBlackhole()
     
 
     @pc.on("datachannel")
@@ -132,39 +63,16 @@ async def offer(request):
         def on_message(message):
             print("Received: ", message)
 
-    # @pc.on("connectionstatechange")
-    # async def on_connectionstatechange():
-    #     log_info("Connection state is %s", pc.connectionState)
-    #     if pc.connectionState == "failed":
-    #         await pc.close()
-    #         pcs.discard(pc)
+    @pc.on("connectionstatechange")
+    async def on_connectionstatechange():
+        log_info("Connection state is %s", pc.connectionState)
+        if pc.connectionState == "failed":
+            await pc.close()
+            pcs.discard(pc)
 
-
-    # @pc.on("track")
-    # def on_track(track):
-    #     log_info("Track %s received", track.kind)
-
-    #     if track.kind == "audio":
-    #         pc.addTrack(player.audio)
-    #         recorder.addTrack(track)
-    #     elif track.kind == "video":
-    #         pc.addTrack(FlagVideoStreamTrack())
-    #         '''
-    #         pc.addTrack(
-    #             VideoTransformTrack(
-    #                 relay.subscribe(track), transform=params["video_transform"]
-    #             )
-    #         )
-    #         '''
-
-    #     @track.on("ended")
-    #     async def on_ended():
-    #         log_info("Track %s ended", track.kind)
-    #         await recorder.stop()
 
     # handle offer
     await pc.setRemoteDescription(offer)
-    #await recorder.start()
 
     # send answer
     answer = await pc.createAnswer()
@@ -192,8 +100,6 @@ async def on_shutdown(app):
 app = web.Application()
 cors = aiohttp_cors.setup(app)
 app.on_shutdown.append(on_shutdown)
-app.router.add_get("/", index)
-app.router.add_get("/client.js", javascript)
 app.router.add_post("/offer", offer)
 
 for route in list(app.router.routes()):
@@ -211,29 +117,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="WebRTC audio / video / data-channels demo"
     )
-    parser.add_argument("--cert-file", help="SSL certificate file (for HTTPS)")
-    parser.add_argument("--key-file", help="SSL key file (for HTTPS)")
     parser.add_argument(
         "--host", default="0.0.0.0", help="Host for HTTP server (default: 0.0.0.0)"
     )
     parser.add_argument(
         "--port", type=int, default=8080, help="Port for HTTP server (default: 8080)"
     )
-    parser.add_argument("--record-to", help="Write received media to a file."),
-    parser.add_argument("--verbose", "-v", action="count")
     args = parser.parse_args()
 
-    if args.verbose:
-        logging.basicConfig(level=logging.DEBUG)
-    else:
-        logging.basicConfig(level=logging.INFO)
-
-    if args.cert_file:
-        ssl_context = ssl.SSLContext()
-        ssl_context.load_cert_chain(args.cert_file, args.key_file)
-    else:
-        ssl_context = None
-
     web.run_app(
-        app, access_log=None, host=args.host, port=args.port, ssl_context=ssl_context
+        app, access_log=None, host=args.host, port=args.port
     )
