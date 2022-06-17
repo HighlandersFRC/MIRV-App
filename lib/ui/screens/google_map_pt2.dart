664322ccb8fa85d779fa6207e8ae9dc676f896c4
zfkhan01 <zfkhan2017@gmail.com>
2022-06-17 12:01:28 -0600

    fixed custom markers + made smaller + aligned polygon on the road

--- lib/ui/screens/google_map_pt2.dart
+++ lib/ui/screens/google_map_pt2.dart
@@ -1,9 +1,8 @@
 import 'dart:collection';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
-import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+
 
 class GooMap extends StatefulWidget {
   
@@ -23,6 +22,17 @@ class GooMap extends StatefulWidget {
 class _GooMapState extends State<GooMap> {
 
 LatLng _cameraCenterLocation = new LatLng(40.47382939771208, -104.96933444375819);
+
+
+BitmapDescriptor mapMarker = BitmapDescriptor.defaultMarker;
+
+
+void setCustomMarker() async {
+  mapMarker = await BitmapDescriptor.fromAssetImage(
+    ImageConfiguration(), 'assets/images/pi_lit_icon.png');
+
+    getMarkers();
+}
     
 LocationData? _locationData;
 
@@ -34,7 +44,7 @@ LocationData? _locationData;
 
   BitmapDescriptor _markerIcon = BitmapDescriptor.defaultMarker;
   GoogleMapController? _googleMapController;
-  var zoom = 16;
+  double zoom = 16.0;
 
   int _polygonIdCounter = 1;
   int _circleIdCounter = 1;
@@ -47,13 +57,37 @@ LocationData? _locationData;
   @override
   void initState(){
     super.initState();
-
-   // _locationData = widget.location;
-    // widget.location.onLocationChanged.listen((event) { if (event.latitude == null|| event.longitude == null) return;
-    //   _googleMapController?.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: LatLng (event.latitude!, event.longitude!), zoom: 16)));});
+    setCustomMarker();
+  
     
-  // widget.location.onLocationChanged.listen((even) { if (event.latitude == null || event.longitude == null) return;
-  //       _googleMapController?.animateCamera(ZoomUpdate.newzoom(CameraPosition(target: (event.zoom))))})
+    setState(() {
+      final String polygonIdVal = 'polygon_id_polygon_1';
+    _polygons.add(Polygon(
+      polygonId: PolygonId(polygonIdVal),
+      points: <LatLng> [ 
+             LatLng(40.47388727274254, -104.96984034776688),
+
+            LatLng(40.473854626951805, -104.9698108434677),
+
+            LatLng(40.473989290736334, -104.96960431337357),
+
+            LatLng(40.47401173467415, -104.96965259313583),
+
+            LatLng(40.47388727274254, -104.96984034776688),
+
+
+],
+      strokeWidth: 2,
+      strokeColor: Colors.yellow,
+      fillColor: Colors.yellow.withOpacity(0.15),
+    ));
+    });
+  widget.location.getLocation().then((value) => _locationData = value);
+    widget.location.onLocationChanged.listen((event) { if (event.latitude == null|| event.longitude == null) return;
+      _googleMapController?.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: LatLng (event.latitude!, event.longitude!), zoom: zoom)));});
+    
+  // widget.location.onLocationChanged.listen((event) { if (event.latitude == null || event.longitude == null) return;
+  //      _googleMapController?.animateCamera(ZoomUpdate.newzoom(CameraPosition(target: (event.zoom))))})
   }
   void _setMarkerIcon() async {
     _markerIcon = await BitmapDescriptor.fromAssetImage(
@@ -146,30 +180,32 @@ LocationData? _locationData;
               target: _cameraCenterLocation,
               zoom: 16,
             ),
+            onCameraMove: (CameraPosition) => zoom = CameraPosition.zoom,
+            zoomGesturesEnabled: true,
             mapType: MapType.hybrid,
             onMapCreated: _onMapCreated,
-            // markers: markers,
-            // circles: _circles,
-            // polygons: _polygons,
-            // myLocationEnabled: true,
-            // onTap: (point) {
-            //   if (_isPolygon) {
-            //     setState(() {
-            //       polygonLatLngs.add(point);
-            //       _setPolygon();
-            //     });
-            //   } else if (_isMarker) {
-            //     setState(() {
-            //       markers.clear();
-            //       _setMarkers(point);
-            //     });
-            //   } else if (_isCircle) {
-            //     setState(() {
-            //       _circles.clear();
-            //       _setCircles(point);
-            //     });
-            //   }
-            // },
+            markers: markers,
+            circles: _circles,
+            polygons: _polygons,
+            myLocationEnabled: true,
+            onTap: (point) {
+              if (_isPolygon) {
+                setState(() {
+                  polygonLatLngs.add(point);
+                  _setPolygon();
+                });
+              } else if (_isMarker) {
+                setState(() {
+                  markers.clear();
+                  _setMarkers(point);
+                });
+              } else if (_isCircle) {
+                setState(() {
+                  _circles.clear();
+                  _setCircles(point);
+                });
+              }
+            },
           ),
           Align(
             alignment: Alignment.bottomCenter,
@@ -260,58 +296,61 @@ LocationData? _locationData;
           )
         ],
       ));
-      late PolylinePoints polylinePoints;
-
-      // List of coordinates to join
-      List<LatLng> polylineCoordinates = [];
-
-      // Map storing polylines created by connecting two points
-      Map<PolylineId, Polyline> polylines = {};
-      _createPolylines(
-    double startLatitude,
-    double startLongitude,
-    double destinationLatitude,
-    double destinationLongitude,
-  ) async {
-    // Initializing PolylinePoints
-    polylinePoints = PolylinePoints();
-
-    // Generating the list of coordinates to be used for
-    // drawing the polylines
-    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
-      Secrets.API_KEY, // Google Maps API Key
-      PointLatLng(startLatitude, startLongitude),
-      PointLatLng(destinationLatitude, destinationLongitude),
-      travelMode: TravelMode.transit,
-    );
-
-    // Adding the coordinates to the list
-    if (result.points.isNotEmpty) {
-      result.points.forEach((PointLatLng point) {
-        polylineCoordinates.add(LatLng(point.latitude, point.longitude));
-      });
     }
+    Set<Marker> getMarkers() { //markers to place on map
+  
+    setState(() { 
+      markers.add(
+        Marker( //add first marker
+        markerId: MarkerId('Marker Title First '),
+        position:  LatLng(40.47389594427804, -104.96977128088474), //position of marker        
+        infoWindow: InfoWindow( //popup info 
+          title: 'Marker Title First ',
+          snippet: 'My Custom Subtitle',
+        ),
+        icon: mapMarker,
+      ));
 
-    // Defining an ID
-    PolylineId id = PolylineId('poly');
+      markers.add(
+        Marker( //add second marker
+        markerId: MarkerId('Marker Title Second '),
+        position: LatLng(40.473914052480914, -104.96976356953382), //position of marker
+        infoWindow: InfoWindow( //popup info 
+          title: 'Marker Title Second ',
+          snippet: 'My Custom Subtitle',
+        ),
+        icon: mapMarker, //Icon for Marker
+      ));
 
-    // Initializing Polyline
-    Polyline polyline = Polyline(
-      polylineId: id,
-      color: Colors.red,
-      points: polylineCoordinates,
-      width: 3,
-    );
+      markers.add(
+        Marker( //add third marker
+        markerId: MarkerId('Marker Title Third '),
+        position: LatLng(40.473932925813926, -104.96975854039192), //position of marker
+        infoWindow: InfoWindow( //popup info 
+          title: 'Marker Title Third ',
+          snippet: 'My Custom Subtitle',
+        ),
+        icon: mapMarker, //Icon for Marker
+      ));
+    
+      markers.add(
+        Marker( //add third marker
+        markerId: MarkerId('Marker Title Third '),
+        position: LatLng(40.473881151657984, -104.96977999806404,), //position of marker
+        infoWindow: InfoWindow( //popup info 
+          title: 'Marker Title Third ',
+          snippet: 'My Custom Subtitle',
+        ),
+        icon: mapMarker, //Icon for Marker
+      ));
+       //add more markers here 
+    });
+  
+    
 
-    // Adding the polyline to the map
-    polylines[id] = polyline;
+    return markers;
   }
-GoogleMap(
-  polylines: Set<Polyline>.of(polylines.values), initialCameraPosition: _cameraCenterLocation
-  // ...
-);
-
-    }
+  
   }
 
 
