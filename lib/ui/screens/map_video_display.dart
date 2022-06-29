87db80aafeb01c12fa7c4ebcb836de1183c5bdc2
zfkhan01 <zfkhan2017@gmail.com>
2022-06-29 15:01:47 -0600

    merged prototype in

--- lib/ui/screens/map_video_display.dart
+++ lib/ui/screens/map_video_display.dart
@@ -2,15 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:rxdart/subjects.dart';
 import 'package:test/models/rover_location.dart';
+import 'package:test/models/rover_metrics.dart';
+import 'package:test/services/mirv_api.dart';
 import 'package:test/ui/screens/rover_operation_map.dart';
 
 class OperationMapVideo extends StatelessWidget {
-  const OperationMapVideo(
+   OperationMapVideo(
       {Key? key, required this.showMap, required this.locationStream, required this.piLitMarkers})
       : super(key: key);
   final bool showMap;
   final BehaviorSubject<LatLng> locationStream;
    final List<RoverLocation> piLitMarkers;
+    final MirvApi _mirvApi = MirvApi();
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +23,15 @@ class OperationMapVideo extends StatelessWidget {
         height: 450,
         child: showMap
             ? RoverOperationMap(locationStream: locationStream, piLitMarkers: piLitMarkers)
-            : ElevatedButton(
+            : 
+              Column(
+               children: [
+                Container(
+                  color: Colors.amber,
+                  width: 800,
+                  height: 450,
+                  child: 
+            ElevatedButton(
                 onPressed: null,
                 style: ButtonStyle(
                   backgroundColor: MaterialStateProperty.all(
@@ -28,6 +39,17 @@ class OperationMapVideo extends StatelessWidget {
                   ),
                 ),
                 child: const Text("video"),
-              ));
+              ),
+              ),
+                StreamBuilder<RoverMetrics>(
+                    stream: _mirvApi.periodicMetricUpdates,
+                    builder: (context, snapshot) {
+                      return Text(snapshot.data != null
+                          ? '${snapshot.data!.telemetry}'
+                          : 'Waiting on data');
+                    }),  
+               ],
+    )
+    );
   }
 }
