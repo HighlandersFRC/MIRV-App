79794f2b98a2a314ebe39a2e5c7546a7aab42d80
zfkhan01 <zfkhan2017@gmail.com>
2022-07-07 15:57:50 -0600

    added basic setting tiles

--- lib/ui/screens/overall_settings.dart
+++ lib/ui/screens/overall_settings.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:settings_ui/settings_ui.dart';
 import 'package:test/ui/screens/app_bar_theme.dart';
 
-class SettingsPage extends StatelessWidget {
-  const SettingsPage({Key? key}) : super(key: key);
+class SettingsPage extends StatefulWidget {
+  @override
+  _SettingsPage createState() => _SettingsPage();
+}
 
+class _SettingsPage extends State<SettingsPage> {
   @override
   Widget build(context) {
     return Scaffold(
@@ -15,6 +19,21 @@ class SettingsPage extends StatelessWidget {
           "settings",
         ),
       ),
+      body: SettingsList(sections: [
+        SettingsSection(title: Text('General'), tiles: [
+          SettingsTile(title: Text('Language'), leading: Icon(Icons.language)),
+          SettingsTile(
+              title: Text('Environment'), leading: Icon(Icons.cloud_queue)),
+        ]),
+        SettingsSection(
+          title: Text('Account'),
+          tiles: [
+            SettingsTile(title: Text('Email'), leading: Icon(Icons.email)),
+            SettingsTile(
+                title: Text('Sign Out'), leading: Icon(Icons.exit_to_app)),
+          ],
+        )
+      ]),
     );
   }
 }
