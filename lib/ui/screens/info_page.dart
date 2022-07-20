aca6e9436fd0ff3befe68bc2ca96e8263e8124dd
zfkhan01 <zfkhan2017@gmail.com>
2022-07-20 17:34:16 -0600

    Update info_page.dart

--- lib/ui/screens/info_page.dart
+++ lib/ui/screens/info_page.dart
@@ -23,46 +23,46 @@ class InfoPage extends StatelessWidget {
                 style: DefaultTextStyle.of(context).style,
                 children: const <TextSpan>[
                   TextSpan(
-                      text: 'About the Multi Incident Response Vehicle (MIRV)',
+                      text: 'About the MIRV Rover: Multi Incident Response Vehicle',
                       style: TextStyle(
                         height: 5,
                         fontFamily: 'Roboto',
                         fontWeight: FontWeight.bold,
-                        fontSize: 26,
+                        fontSize: 22,
                         color: Colors.blue,
                         decoration: TextDecoration.underline,
                         decorationColor: Colors.white,
                       )),
                   TextSpan(
                       text:
-                          '\nThe Multi Incident Response Vehicle MIRV is a rover that can navigate, deploy, and recover road flares from a level 4 autonomous F-150 truck, both autonomously or remote-controlled.\nThe MIRV rover is built using the Robot Operating System ROS. The rover utilizes cameras, GPS, and LiDAR to retrieve and deploy Pi-Lit flares. The rover is designed to maneuver on rough highway shoulders. The MIRV rover will also operate autonomously or by remote control.',
+                          '\nThe Multi Incident Response Vehicle (MIRV) is a rover that can navigate, deploy, and recover road flares from a level 4 autonomous F-150 truck, both autonomously or remote-controlled.\nThe MIRV rover is built using the Robot Operating System (ROS). The rover utilizes cameras, GPS, and LiDAR to retrieve and deploy Pi-Lit flares. The rover is designed to maneuver on rough highway shoulders. The MIRV rover will also operate autonomously or by remote control.',
                       style: TextStyle(
                         height: 2,
                         fontFamily: 'Roboto',
-                        fontSize: 13,
+                        fontSize: 16,
                         fontWeight: FontWeight.normal,
                         color: Colors.black,
                         decoration: TextDecoration.underline,
                         decorationColor: Colors.white,
                       )),
                   TextSpan(
-                      text: '\nAbout the company',
+                      text: '\nAbout Neaera Consulting',
                       style: TextStyle(
                         height: 5,
                         fontFamily: 'Roboto',
                         fontWeight: FontWeight.bold,
-                        fontSize: 26,
+                        fontSize: 22,
                         color: Colors.blue,
                         decoration: TextDecoration.underline,
                         decorationColor: Colors.white,
                       )),
                   TextSpan(
                       text:
-                          '\nNeaera Consulting Group was started in 2004, with an emphasis on Network integration, Security services, and application development in the insurance industry. They have now moved on to services within the Medical, Insurance, and Government agencies.',
+                          '\nNeaera Consulting Group was started in 2004, with an emphasis on Network integration, Security services, and application development in the insurance industry. In the past 5 years, Neaera has moved into the Connected and Automated vehicle industries working with USDOT, WYDOT and other transportation entities.  ',
                       style: TextStyle(
                         height: 2,
                         fontFamily: 'Roboto',
-                        fontSize: 13,
+                        fontSize: 16,
                         color: Colors.black,
                         fontWeight: FontWeight.normal,
                         decoration: TextDecoration.underline,
