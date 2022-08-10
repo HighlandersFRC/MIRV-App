2feaf658f25883183cd9a83b45a0ef2a809ddff2
zfkhan01 <zfkhan2017@gmail.com>
2022-08-10 11:15:09 -0600

    added garage_id to the op page bar

--- lib/ui/screens/garage-pages/garage_app_bar.dart
+++ lib/ui/screens/garage-pages/garage_app_bar.dart
@@ -1,48 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mirv/models/garage/garage_metrics.dart';
+import 'package:mirv/services/mirv_api.dart';
 import 'package:mirv/ui/screens/garage-pages/garage_status_bar.dart';
 import 'package:mirv/ui/screens/garage_status_page.dart';
 
 class GarageAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const GarageAppBar({
+  GarageAppBar({
     Key? key,
     required this.garageMetricsObs,
   }) : super(key: key);
   final Rx<GarageMetrics?> garageMetricsObs;
+  final MirvApi _mirvGarageApi = MirvApi();
 
   @override
   Size get preferredSize => const Size.fromHeight(60.0);
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(title: Obx(() => GarageStatusBar(garageMetrics: garageMetricsObs.value!)), actions: [
-      Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ElevatedButton(
-          onPressed: () => showDialog(
-            context: context,
-            builder: (BuildContext context) {
-              return AlertDialog(
-                content: AspectRatio(aspectRatio: 1.5, child: Obx(() => StatusPageGarage(garageMetricsObs.value!))),
-                actions: [
-                  TextButton(
-                    onPressed: () {
-                      return Navigator.pop(context);
-                    },
-                    child: const Text('Close'),
-                  )
-                ],
-              );
-            },
+    return AppBar(
+        title: Obx(() => GarageStatusBar(
+              garageMetrics: garageMetricsObs.value!,
+              garageMetricsObs: _mirvGarageApi.garageMetricsObs,
+            )),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              onPressed: () => showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    content: AspectRatio(aspectRatio: 1.5, child: Obx(() => StatusPageGarage(garageMetricsObs.value!))),
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          return Navigator.pop(context);
+                        },
+                        child: const Text('Close'),
+                      )
+                    ],
+                  );
+                },
+              ),
+              style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.blue[700])),
+              child: const Text(
+                " Status ",
+                textScaleFactor: 2.5,
+              ),
+            ),
           ),
-          style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.blue[700])),
-          child: const Text(
-            " Status ",
-            textScaleFactor: 2.5,
-          ),
-        ),
-      ),
-    ]);
+        ]);
   }
 }
--- lib/ui/screens/garage-pages/garage_status_bar.dart
+++ lib/ui/screens/garage-pages/garage_status_bar.dart
@@ -1,28 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:mirv/models/garage/garage_metrics.dart';
 import 'package:mirv/models/garage/garage_state_type.dart';
 
 class GarageStatusBar extends StatelessWidget {
   GarageStatusBar({
     Key? key,
-    required this.garageMetrics,
+    required this.garageMetrics, required this.garageMetricsObs,
   }) : super(key: key);
   final GarageMetrics garageMetrics;
+  final Rx<GarageMetrics?> garageMetricsObs;
 
-  StatelessWidget _stateDescription(GarageStateType state) {
+  StatelessWidget _stateDescription(GarageStateType state, GarageMetrics garageMetrics) {
     switch (state) {
       case GarageStateType.retracted_unlatched:
-        return const Text("State: Retracted_unlatched", textScaleFactor: 1);
+        return  Text("${garageMetricsObs.value!.garage_id}   State: Retracted_unlatched", textScaleFactor: 1);
       case GarageStateType.retracted_latched:
-        return const Text("State: Retracted_latched", textScaleFactor: 1);
+        return  Text("${garageMetricsObs.value!.garage_id}   State: Retracted_latched", textScaleFactor: 1);
       case GarageStateType.deployed:
-        return const Text("State: Deployed", textScaleFactor: 1);
+        return  Text("${garageMetricsObs.value!.garage_id}   State: Deployed", textScaleFactor: 1);
       case GarageStateType.unavailable:
-        return const Text("State: Unavailable", textScaleFactor: 1);
+        return  Text("${garageMetricsObs.value!.garage_id}   State: Unavailable", textScaleFactor: 1);
       case GarageStateType.in_motion_retract:
-        return const Text("State: Retraction in progress", textScaleFactor: 1);
+        return  Text("${garageMetricsObs.value!.garage_id}   State: Retraction in progress", textScaleFactor: 1);
       case GarageStateType.in_motion_deploy:
-        return const Text("State: Deploy in progress", textScaleFactor: 1);
+        return  Text("State: Deploy in progress", textScaleFactor: 1);
     }
   }
 
@@ -31,7 +33,7 @@ class GarageStatusBar extends StatelessWidget {
     BuildContext context,
   ) {
     return Row(mainAxisSize: MainAxisSize.min, children: [
-      _stateDescription(garageMetrics.state),
+      _stateDescription(garageMetrics.state, garageMetricsObs.value!),
     ]);
   }
 }
